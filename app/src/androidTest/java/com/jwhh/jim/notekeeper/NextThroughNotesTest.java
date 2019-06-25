44738424c808314d5b40b4dbe36856cbd3582f80
ANONYMOUS-HUMANOID-DEVELOPER <glenyufui@gmail.com>
2019-06-25 02:20:04 +0100

    Added instrumentation tests for checking of note list, and clickable menu items behaviour.

--- app/src/androidTest/java/com/jwhh/jim/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/jwhh/jim/notekeeper/NextThroughNotesTest.java
@@ -14,9 +14,11 @@ import java.util.List;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
 
 public class NextThroughNotesTest {
     @Rule
@@ -30,12 +32,16 @@ public class NextThroughNotesTest {
         onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        int index  = 0;
-        NoteInfo note  = notes.get(index);
+        for (int index  = 0; index < notes.size(); index++) {
+            NoteInfo note = notes.get(index);
 
-        onView(withId(R.id.spinner_courses)).check(
-                matches(withSpinnerText(note.getCourse().getTitle())));
-        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
-        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+            onView(withId(R.id.spinner_courses)).check(
+                    matches(withSpinnerText(note.getCourse().getTitle())));
+            onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+            onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+            if (index < notes.size() - 1)
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click());
+        }
     }
 }
